2ef0a257889c9c107d2f69c1aefd712d69b8024b
Omkar <omkarrajan495@gmail.com>
2024-10-27 21:20:07 +0530

    time complexity starter

---
+++ starter/timeComplexity/code.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
+
+//Time Complexities(
+// 1. Constant --> O(1) || O(k) which is parrallel to x axis and is the most complex time.
+// 2. Linear --> O(n) loop runs same time as the n , so x=y .
+// 3. --> O(n^2) || O(n^3) generally on 2 || 3 nested loop  , the complexity of n running is double
+// 4. ---> O(logn) second best time complexity , generally if in any algo we encounter situation where the arr or val is decreasing to half to find solution0 .
+// 5. ---> n(logn)
