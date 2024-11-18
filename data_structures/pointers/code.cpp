3dbe497ddf20a400d26cda28a9bb2fefe047e3d3
Omkar <omkarrajan495@gmail.com>
2024-11-18 22:50:19 +0530

    pointers

--- data_structures/pointers/code.cpp
+++ data_structures/pointers/code.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a = 10;
-    float f = 12.99;
-    int* ptr = &a;
-    int** parPtr = &ptr;
+void changeA(int &ptr){
+    ptr = 7;
+}
 
-    int *ppt = NULL;
+int main(){
+    // int a = 10;
+    // float f = 12.99;
+    // int* ptr = &a;
+    // int** parPtr = &ptr;
+
+    // int *ppt = NULL;
+    // changeA(a);
+
+    // int b = 10;
+    // int *p = &b;
+    // cout << p << endl;
+    // p++;
+    // cout << p << endl;
+
+    int arr[] = {1,2,3,4,5};
+    // int *ptr = arr;
+    int ptr = *arr; //which will give first index ; // 1
+    int ptr2 = *(arr+1); //which will give second index ; // 2
+
+    int* ponter = arr;
 
     // cout << &a  << " " << **(&ptr) << endl;
     // cout << &a  << " " << *(ptr) << endl;
-    cout << ppt << endl;
+    cout << *ponter << endl;
+    cout << *(ponter+1) << endl;
+    cout << *ponter << endl; //Derefernce so it will output the value
     return 0;
 }
 
@@ -36,5 +56,26 @@ int main(){
 //Used when --> LinkedList, Trees
 
 //Pass By Reference
+// 1st Method
 //Generally if a fuction needs a parameter and call it we pass the parameter and it is passed by reference, i.e its copy is stored
-//But if we pass the address of the parameter [variable] by passing pointer of 'a' or memory address of 'a' i.e. &a
+//But if we pass the address of the parameter [variable] by passing pointer of 'a' or memory address of ' a' i.e. &a
+// 2nd Method (alias)
+//for eg: 'Tony Stark' or 'Iron Man' both are same person
+//If we pass variable on another name in my function and while calling if we add our main variable it will be by refernce ,
+//only while defining the func use '&' i.e. alias with the other arg name
+//Example fun(&b) will be storing same address of a when func(a)
+
+
+//Array Pointer
+//Array itself is a pointer and called as constant pointer, so if tried to modifiy any value of arr with a variable it will give an error.
+
+//Pointer Arithmetic
+//.Increment -->
+//when a++ then the value gets increment but the addreess adds additional 4 byte which it incremented
+//eg: a=10 *p = &a == 100; when a++; then a=11, p=104
+//.Decrement -->
+//Similarly for decrement when a-- then the value gets decrement but the addreess subracts additional 4 byte which it decremented 
+//eg: a=10 *p = &a == 100; when a--; then a=9, p=96
+
+//Adding two pointers is not allowed, but subracting can be done with same type
+//Also comparing two pointers '< > = '
