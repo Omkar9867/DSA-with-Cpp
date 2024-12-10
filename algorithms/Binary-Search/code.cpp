1c1c658f65a42a24e774657104eaecca713ee513
Omkar <omkarrajan495@gmail.com>
2024-12-10 23:07:13 +0530

    tc of binary-search

--- algorithms/Binary-Search/code.cpp
+++ algorithms/Binary-Search/code.cpp
@@ -2,15 +2,32 @@
 #include <vector>
 using namespace std;
 
-//My Approach by learning
+// //My Approach by learning
+// int binary_search(vector<int>sortedArr, int target){
+//     int st = 0, ed = sortedArr.size() -1;
+//     while(st <= ed) {
+//         int mid = (st + ed) /2;
+//         if(sortedArr[mid] > target){
+//             ed = mid - 1;
+//         }else if(sortedArr[mid] < target){
+//             st = mid+1;
+//         }else{
+//             return mid;
+//         }
+//     }
+//     return  -1;
+// }
+
+// --------------------------------------------TC - O(logn) ------------------------------------
+
 int binary_search(vector<int>sortedArr, int target){
     int st = 0, ed = sortedArr.size() -1;
     while(st <= ed) {
-        int mid = (st + ed) /2;
+        int mid = st + (ed - st) /2;
         if(sortedArr[mid] > target){
-            ed = mid - 1;
+            ed = mid - 1; //1st Half
         }else if(sortedArr[mid] < target){
-            st = mid+1;
+            st = mid+1; //2nd Half
         }else{
             return mid;
         }
@@ -20,7 +37,7 @@ int binary_search(vector<int>sortedArr, int target){
 
 int main(){
     vector<int> arr = {-1, 0, 3, 4, 5, 9, 12};
-    int target = 9;
+    int target = 5;
     int value = binary_search(arr, target);
 
     cout << "Index value of Target : " << value << endl;
@@ -31,3 +48,16 @@ int main(){
 //Basically the search alorithm is to search a target in given array or string.
 //Binary search is over the Linear Search Algorithm which means it is more enhanced/optimised then Linear-Search.
 //There is a specific demand to use the Binary Search and i.e. sort(); which means it can be applied to a sorted array only.
+
+//Note:
+//int mid = (st + ed) /2;  
+//Formula of calculating mid value the worst case scenario of the st and ed 'int' value could be INT_MAX , and adding the
+//max int value can lead to overflow.
+//SOlution: ALternate fomula:
+//mid = st + (end-st)/2 ---> Here it result the same but approach is first subracting the max_int then dividing and eventually adding.
+
+// how did the time complexity is O(logn)?
+// ---> At every operation our array size get half i.e n/2 ==> n/4 ==> n/8
+// which can also state as : n/2^1 ==> n/2^2 ==> n/2^3 ------Till 1 i.e k
+// So n/2^k wil require to get 1; where can also be written as:
+// n/2^k   ==   n=2^k   ==  log2n = k where base (2) is ignored.
