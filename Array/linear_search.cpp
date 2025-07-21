ab2b1130917472c6aeb0a09af2a8275782bc6ef8
Omkar9867 <omkarrajan495@gmail.com>
2025-07-21 22:23:36 +0530

    refreshed

--- Array/linear_search.cpp
+++ Array/linear_search.cpp
@@ -4,17 +4,6 @@ using namespace std;
 
 //-----------------------------------BruteForce Approach O(n)-------------------
 
-// int findPresentNum(vector<int> arr, int num){
-//     for(int i = 0; i < arr.size(); i++){
-//         if(arr[i] == num){
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
-
-// -------------------------------- 
-
 int findPresentNum(vector<int> arr, int num){
     for(int i = 0; i < arr.size(); i++){
         if(arr[i] == num){
@@ -23,6 +12,7 @@ int findPresentNum(vector<int> arr, int num){
     }
     return -1;
 }
+
 
 int main(){
     vector<int> arr = {1, 2, 3, 4, 5};
