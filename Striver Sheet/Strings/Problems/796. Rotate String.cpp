1ae66fe8b095d9d522df6e71fba554d93e80d2a1
Omkar9867 <omkarrajan495@gmail.com>
2025-10-24 18:32:44 +0530

    bruteforce & optimal approach

--- Striver Sheet/Strings/Problems/796. Rotate String.cpp
+++ Striver Sheet/Strings/Problems/796. Rotate String.cpp
@@ -2,24 +2,50 @@
 #include <algorithm>
 
 // ----------------My approach
-bool rotateString(std::string s, std::string goal) {
-    int indexNo;
-    for (int i = 0; i < goal.size(); i++){
-        if(s[0] == goal[i]){
-            indexNo = i;
-            break;
-        }
-    }
-    int i = 0;
-    std::reverse(s.begin(), s.end());
-    std::reverse(s.begin(), s.begin() + indexNo);
-    std::reverse(s.begin() + indexNo, s.end());
+// bool rotateString(std::string s, std::string goal) {
+//     int indexNo;
+//     for (int i = 0; i < goal.size(); i++){
+//         if(s[0] == goal[i]){
+//             indexNo = i;
+//             break;
+//         }
+//     }
+//     int i = 0;
+//     std::reverse(s.begin(), s.end());
+//     std::reverse(s.begin(), s.begin() + indexNo);
+//     std::reverse(s.begin() + indexNo, s.end());
     
-    if(s == goal){
-        return true;
-    }else{
+//     if(s == goal){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// -------------------------------Correct Approaches-----------------------------------------------
+// -------------------------------Brute Force-----------------------------------------------
+// bool rotateString(std::string s, std::string goal) {
+    //     int indexNo = 0;
+    //     while (indexNo < goal.size()){
+        //         std::string rotate = s.substr(indexNo) + s.substr(0, indexNo);
+        //         std::cout << "Rotate in While " << rotate << std::endl;
+        //         std::cout << "S in While " << s << std::endl;
+        //         if(rotate == goal){
+            //             return true;
+            //         }
+            //         indexNo ++;
+            //     }
+            //     return false;
+            // }
+            
+// -------------------------------Optimal Force--------------------------------------------
+
+bool rotateString(std::string s, std::string goal) {
+    if(s.length() != goal.length()){
         return false;
     }
+    std::string doubleS = s + s;
+    return doubleS.find(goal) != std::string::npos; // npos means not found or no position
 }
 
 int main(){
