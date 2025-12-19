858101a63365d1ba4d17bdadc3afca98279251b3
Omkar9867 <omkarrajan495@gmail.com>
2025-12-19 23:38:51 +0530

    cycle LL optimal approach

--- Striver Sheet/LinkedList/Single LL/Single LL Problems/141. Linked List Cycle.cpp
+++ Striver Sheet/LinkedList/Single LL/Single LL Problems/141. Linked List Cycle.cpp
@@ -18,25 +18,33 @@ struct Node{
 class Solution{
 public:
     //-------------------------------------------------Brute Force Approach TC->O(N) -- SC->O(N) -------------------
+    // bool hasCycle(Node *head) {
+    //     std::unordered_map<Node*, int> hashedVal;
+    //     Node* temp = head;
+    //     while(temp != NULL){
+    //         if (hashedVal.find(temp) != hashedVal.end()) {
+    //             return true;
+    //         }
+    //         hashedVal[temp] = 1;
+    //         temp = temp->next;
+    //     }
+    //     return false;
+    // }
+
+    // ---------------------------------------------- Optimal Approach TC->O(N) -- SC->O(1)------------------------------
+    //*  Tortoise and Hare Algorithm (Floyd’s Cycle Detection).  //
     bool hasCycle(Node *head) {
-        std::unordered_map<Node*, int> hashedVal;
-        Node* temp = head;
-        while(temp != NULL){
-            if (hashedVal.find(temp) != hashedVal.end()) {
+        Node* slow = head;
+        Node* fast = head;
+        while(fast != NULL && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
                 return true;
             }
-            hashedVal[temp] = 1;
-            temp = temp->next;
         }
         return false;
     }
-
-    // ---------------------------------------------- Optimal Approach TC->O(N) -- SC->O(1)------------------------------
-    bool hasCycle(Node *head) {
-        
-    }
-
-    // ---------------------------------------------- Recursive Approach TC->O(N) -- SC->O(1)------------------------------
 
     void printList(Node* head){
         // Node* temp = head;
@@ -51,14 +59,12 @@ public:
 int main(){
     Solution sol;
 
-    Node* head = new Node(1);
+    Node* head = new Node(3);
     head->next = new Node(2);
-    head->next->next = new Node(3);
-    head->next->next->next = new Node(4);
-    head->next->next->next->next = new Node(5);
+    head->next->next = new Node(0);
+    head->next->next->next = new Node(-4);
 
-    // Node* result = sol.reverseList(head);
-    // std::cout << "Reverse of the Linked List: " << std::endl;
-    // sol.printList(result);
+    bool result = sol.hasCycle(head);
+    std::cout << "Result of cycle LL is : "<< result << std::endl;
     return 0;
 }
