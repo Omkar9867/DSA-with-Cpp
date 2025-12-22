e8a1476f0a06543a4bf3727106bd926f01fa0584
Omkar9867 <omkarrajan495@gmail.com>
2025-12-22 22:30:24 +0530

    optimal solution for palindrome LL

--- Striver Sheet/LinkedList/Single LL/Single LL Problems/234. Palindrome Linked List.cpp
+++ Striver Sheet/LinkedList/Single LL/Single LL Problems/234. Palindrome Linked List.cpp
@@ -18,37 +18,69 @@ struct Node{
 class Solution{
 public:
     //-------------------------------------------------Brute Force Approach TC->O(N) -- SC->O(N) -------------------
+    // bool isPalindrome(Node *head) {
+    //     std::stack<int> st;
+
+    //     Node* temp = head;
+
+    //     // Traverse the linked list and push values onto the stack
+    //     while (temp != NULL) {
+    //         st.push(temp->val);
+    //         temp = temp->next; 
+    //     }
+
+    //     // Reset
+    //     temp = head;
+
+    //     // Compare values by popping from the stack and checking against linked list nodes
+    //     while (temp != NULL) {
+    //         if (temp->val != st.top()) {
+    //             return false;
+    //         }
+    //         st.pop();
+    //         temp = temp->next; 
+    //     }
+
+    //     // If all values match, it's a palindrome
+    //     return true;
+    // }
+
+    // ---------------------------------------------- Optimal Approach TC->O(N) -- SC->O(1)------------------------------
+    // Space Complexity: O(1), no extra space is used apart from a few pointers; operations are done in-place.
+    // Key Solution: After finding the middle, then reversing the second have and looping from head to middle and (reversed)->middle to tail
     bool isPalindrome(Node *head) {
-        std::stack<int> st;
-
-        Node* temp = head;
-
-        // Traverse the linked list and push values onto the stack
-        while (temp != NULL) {
-            st.push(temp->val);
-            temp = temp->next; 
+        if (head == NULL || head->next == NULL) {
+            return true;  // It's a palindrome by definition
         }
-
-        // Reset
-        temp = head;
-
-        // Compare values by popping from the stack and checking against linked list nodes
-        while (temp != NULL) {
-            if (temp->val != st.top()) {
+        Node* fast = head;
+        Node* slow = head;
+        Node* temp = head;
+        while(fast != NULL && fast->next != nullptr){ //1. Find the middle
+            slow = slow->next;
+            fast = fast->next->next;
+        };
+        Node* reversedNode = reverseNode(slow); //2. reverse the second half from the middle
+        while(temp != slow && reversedNode != NULL){ //3. compare from temp i.e. head to middle --to-- middle to tail
+            if(temp->val != reversedNode->val){
                 return false;
             }
-            st.pop();
-            temp = temp->next; 
+            temp = temp->next;
+            reversedNode = reversedNode->next;
         }
-
-        // If all values match, it's a palindrome
         return true;
     }
 
-    // ---------------------------------------------- Optimal Approach TC->O(N) -- SC->O(1)------------------------------
-    // bool isPalindrome(Node *head) {
-
-    // }
+    Node* reverseNode(Node* head){
+        Node* temp = head;
+        Node* prev = NULL;
+        while(temp != NULL){
+            Node* front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        };
+        return prev;
+    }
 
     void printList(Node* head){
         // Node* temp = head;
@@ -63,14 +95,16 @@ public:
 int main(){
     Solution sol;
 
-    Node* head = new Node(3);
+    Node* head = new Node(1);
     head->next = new Node(2);
     Node* nodeSecond = head->next;
     // std::cout << nodeSecond->val << std::endl;
-    head->next->next = new Node(0);
-    head->next->next->next = new Node(-4);
+    head->next->next = new Node(2);
+    head->next->next->next = new Node(1);
 
     bool result = sol.isPalindrome(head);
     std::cout << "Result of isPalindrome is : "<< result << std::endl;
+    // Node* reversedNode = sol.reverseNode(head);
+    // sol.printList(reversedNode);
     return 0;
 }
