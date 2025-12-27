c7ca840ac8238f0e56d852fbc7d5629791628287
Omkar9867 <omkarrajan495@gmail.com>
2025-12-27 20:46:54 +0530

    maximum subarray

---
+++ Striver Sheet/Array/Problems/53. Maximum Subarray.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+//------------------------------Optimal Approach--TC->O(N)--SC->(1)------------------------
+// int maxSubArray(std::vector<int>& nums){
+//     int maxSum = INT_MIN;
+//     int sum = nums[0];
+//     for(int i = 0; i < nums.size(); i++){
+//         sum = std::max(nums[i], sum + nums[i]);
+//         maxSum = std::max(maxSum, sum);
+//     }
+//     return maxSum;
+// }
+
+//------------------------------Kadane's Algorithm--TC->O(N)--SC->(1)------------------------
+int maxSubArray(std::vector<int>& nums){
+    int maxSum = INT_MIN;
+    int sum = 0;
+    for(int i = 0; i < nums.size(); i++){
+        sum += nums[i];
+        if(sum > maxSum){
+            maxSum = sum;
+        }
+        if(sum < 0){  //-------Main kadan's addon --(Is it better to start a new subarray here, or continue the previous one?)
+            sum = 0;
+        }
+    }
+    return maxSum;
+}
+
+int main(){
+    std::vector<int> arr = {-2,1,-3,4,-1,2,1,-5,4};
+    int result = maxSubArray(arr);
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
+
+// 53. Maximum Subarray
+
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+// Example 1:
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+// Example 2:
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+
+// Example 3:
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+// Constraints:
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+ 
+
+//! Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
