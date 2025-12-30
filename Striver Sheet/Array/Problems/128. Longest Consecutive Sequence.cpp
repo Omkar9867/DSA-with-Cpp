577172af10a2d9a209254d6f9a11ac73824ebdb7
Omkar9867 <omkarrajan495@gmail.com>
2025-12-30 23:10:01 +0530

    Optimal Approach

--- Striver Sheet/Array/Problems/128. Longest Consecutive Sequence.cpp
+++ Striver Sheet/Array/Problems/128. Longest Consecutive Sequence.cpp
@@ -1,23 +1,51 @@
 #include <bits/stdc++.h>
 
 // ------------------------------------Better Approach--TC->O(nlogn)--SC->O(1)---------------------------
+// int longestConsecutive(std::vector<int>& nums) {
+//     if(nums.size() == 0) return 0;
+//     std::sort(nums.begin(), nums.end());
+//     int cnt = 0, maxCnt = 1;
+//     int lastSmaller = INT_MIN;
+//     for(int i = 0; i < nums.size(); i++){
+//         if(nums[i] - 1 == lastSmaller){ // 1. If the number - 1 == previous , so the sequence is ready 
+//             cnt++;
+//             lastSmaller = nums[i];
+//         }else if(nums[i] != lastSmaller){ // 2. if the current number is not same , means no sequence, Incase of same, count will not add sequence will continue
+//             cnt = 1; 
+//             lastSmaller = nums[i];
+//         }
+//         maxCnt = std::max(cnt, maxCnt);
+//     }
+//     return maxCnt;
+// }
+
+// ------------------------------------Optimal Approach--TC->O(N)--SC->O(N)---------------------------
 int longestConsecutive(std::vector<int>& nums) {
-    if(nums.size() == 0) return 0;
-    std::sort(nums.begin(), nums.end());
-    int cnt = 0, maxCnt = 1;
-    int lastSmaller = INT_MIN;
-    for(int i = 0; i < nums.size(); i++){
-        if(nums[i] - 1 == lastSmaller){ // 1. If the number - 1 == previous , so the sequence is ready 
-            cnt++;
-            lastSmaller = nums[i];
-        }else if(nums[i] != lastSmaller){ // 2. if the current number is not same , means no sequence, Incase of same, count will not add sequence will continue
-            cnt = 1; 
-            lastSmaller = nums[i];
-        }
-        maxCnt = std::max(cnt, maxCnt);
+    int n = nums.size();
+    if(n == 0) return 0;
+
+    int maxCnt = 1;
+    std::unordered_set<int> st;
+    for(int i = 0; i < n; i++){
+        st.insert(nums[i]);
+    }
+    // Traverse the set to find the longest sequence
+    for(auto it : st){
+        if(st.find(it - 1) == st.end()){ // if the value sequence is not found. It is starting num of sequence
+            int cnt = 1;
+            int x = it; // Starting element of the sequence
+            while(st.find(x+1) != st.end()){
+                x++;
+                cnt ++;
+            }
+            maxCnt = std::max(maxCnt, cnt);
+        }// sequence count condition will not run until some connection is found
     }
     return maxCnt;
 }
+
+// Think like, i want to run a sequence when the num - 1 is not found, and when it is found, i will try to count the sequence. if the sequence break some count may or may not be stored , thats the main answer we want
+
 
 
 int main(){
