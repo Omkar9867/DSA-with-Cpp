6a24cbe0c7b9b6aa12253403d68832cdce3c88d0
Omkar9867 <omkarrajan495@gmail.com>
2026-01-07 22:45:20 +0530

    refubished approach

--- Striver Sheet/Binary/find_how_namy_time_array_rotated.cpp
+++ Striver Sheet/Binary/find_how_namy_time_array_rotated.cpp
@@ -2,33 +2,51 @@
 #include <vector>
 
 // ------------------------------------My Approach----------------------------------
+// int rotatedArrNum(std::vector<int>& nums){
+//     int n = nums.size();
+//     int st = 0, ed = n - 1;
+//     if(nums[st] < nums[ed]){
+//         return 0; // handled condition where already sorted and returned the index
+//     }
+//     while(st<=ed){
+//         int mid = st + (ed - st)/2;
+//         // if around mid is the actual answer
+//         if(nums[mid] > nums[mid + 1]){
+//             return mid + 1;
+//         }
+//         if(nums[mid - 1] > nums[mid]){
+//             return mid;
+//         }
+//         // by decreasing the find length the mid will give us answer eventually
+//         if(nums[mid] > nums[st]){
+//             st = mid + 1;
+//         }else{
+//             ed = mid - 1;
+//         }
+//     }
+//     return 0;
+// }
+// ------------------------------------Refurbished Approach----------------------------------
 int rotatedArrNum(std::vector<int>& nums){
     int n = nums.size();
     int st = 0, ed = n - 1;
     if(nums[st] < nums[ed]){
         return 0; // handled condition where already sorted and returned the index
     }
-    while(st<=ed){
+    while(st<ed){
         int mid = st + (ed - st)/2;
-        // if around mid is the actual answer
-        if(nums[mid] > nums[mid + 1]){
-            return mid + 1;
-        }
-        if(nums[mid - 1] > nums[mid]){
-            return mid;
-        }
-        // by decreasing the find length the mid will give us answer eventually
-        if(nums[mid] > nums[st]){
+        std::cout << "Mid: " << mid << std::endl;
+        if(nums[mid] > nums[ed]){
             st = mid + 1;
         }else{
-            ed = mid - 1;
+            ed = mid; // Here rather then setting ed to mid - 1,It will be only ed
         }
     }
-    return 0;
+    return st;
 }
 
 int main(){
-    std::vector<int> arr = {4,5,6,7,0,1,2,3};
+    std::vector<int> arr = {4,5,6,0,1,2,3};
     int result = rotatedArrNum(arr);
     std::cout<< "Result: " << result << std::endl;
     return 0;
