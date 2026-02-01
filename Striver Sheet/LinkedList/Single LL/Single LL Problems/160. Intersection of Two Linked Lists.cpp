9010247e358e7dd6778cfc4b4c97d78c0f068fa2
Omkar9867 <omkarrajan495@gmail.com>
2026-02-01 21:12:35 +0530

    Intersection of two list , better and optimal solution

--- Striver Sheet/LinkedList/Single LL/Single LL Problems/160. Intersection of Two Linked Lists.cpp
+++ Striver Sheet/LinkedList/Single LL/Single LL Problems/160. Intersection of Two Linked Lists.cpp
@@ -18,17 +18,62 @@ struct Node{
 class Solution{
 public:
     //-------------------------------------------------Brute Force Approach TC->O(N^2) -- SC->O(1) -------------------
+    // Node* getIntersectionNode(Node* headA, Node* headB){
+    //     while(headB != nullptr){
+    //         Node* temp = headA;
+    //         while (temp != nullptr){
+    //             if(temp == headB){ //! Note donot check value, check node instead 
+    //                 return headB;
+    //             }else{
+    //                 temp = temp->next;
+    //             }
+    //         }         
+    //         headB = headB->next;
+    //     }
+    //     return NULL;
+    // }
+
+    //-------------------------------------------------Better Approach TC->O(N) -- SC->O(N) --//*Hashing*-----------------
+    // Node* getIntersectionNode(Node* headA, Node* headB){
+    //     std::unordered_set<Node*> st;
+    //     while(headA != nullptr){
+    //         st.insert(headA);
+    //         headA = headA->next;
+    //     }
+    //     while(headB != nullptr){
+    //         if(st.find(headB) != st.end()) return headB;
+    //         headB = headB->next;
+    //     }
+    //     return NULL;
+    // }
+
+    //-------------------------------------------------Optimal Approach TC->O(N) -- SC->O(1) --//*Hashing*-----------------
+    int getDifference(Node* head1, Node* head2) {
+        int len1 = 0, len2 = 0;
+        while (head1 != NULL || head2 != NULL) {
+            if (head1 != NULL) {
+                ++len1;
+                head1 = head1->next;
+            }
+            if (head2 != NULL) {
+                ++len2;
+                head2 = head2->next;
+            }
+        }
+        return len1 - len2;  // If negative, length of list2 > length of list1, else vice-versa
+    }
     Node* getIntersectionNode(Node* headA, Node* headB){
-        while(headB != nullptr){
-            Node* temp = headA;
-            while (temp != nullptr){
-                if(temp == headB){ //! Note donot check value, check node instead 
-                    return headB;
-                }else{
-                    temp = temp->next;
-                }
-            }         
+        int diff = getDifference(headA, headB);
+    
+        if (diff < 0) 
+            while (diff++ != 0) headB = headB->next;
+        else 
+            while (diff-- != 0) headA = headA->next;
+
+        while(headA != nullptr){
+            if (headA == headB) return headA;  // Intersection point found
             headB = headB->next;
+            headA = headA->next;
         }
         return NULL;
     }
