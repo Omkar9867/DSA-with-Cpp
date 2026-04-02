81cf038df91550c88c44e6eb96350fdbc4095029
Omkar9867 <omkarrajan495@gmail.com>
2026-04-03 00:04:06 +0530

    largest triangle in historgam bruteforce

---
+++ Striver Sheet/Stack_and_Queues/Problems/84. Largest Rectangle in Histogram.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+//---------------------------Brute Force Approach-TC->O(N^2)-------------------------
+int largestRectangleArea(std::vector<int>& heights) {
+    int maxArea = 0;  // Variable to store maximum area found
+    int n = heights.size();
+    for (int i = 0; i < n; i++) {
+        int minHeight = INT_MAX;
+
+        // Inner loop to set the ending index of the subarray
+        for (int j = i; j < n; j++) {
+            // Update minimum height in the current window
+            minHeight = std::min(minHeight, heights[j]);
+
+            // Calculate area = height * width(how many times)
+            int width = j - i + 1;
+            int area = minHeight * width;
+
+            // Update maxArea if this area is larger
+            maxArea = std::max(maxArea, area);
+        }
+    }
+
+    return maxArea;
+}
+
+//---------------------------Optimised Approach 1-TC->O(N^2)-------------------------
+int largestRectangleArea(std::vector<int>& heights) {
+    
+}
+
+// 84. Largest Rectangle in Histogram
+// Hard
+
+// Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+// Example 1:
+// Input: heights = [2,1,5,6,2,3]
+// Output: 10
+// Explanation: The above is a histogram where width of each bar is 1.
+// The largest rectangle is shown in the red area, which has an area = 10 units.
+
+// Example 2:
+// Input: heights = [2,4]
+// Output: 4
+
+// Constraints:
+// 1 <= heights.length <= 105
+// 0 <= heights[i] <= 104
