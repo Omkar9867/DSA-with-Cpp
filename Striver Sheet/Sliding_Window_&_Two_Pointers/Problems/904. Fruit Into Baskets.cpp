b7be1f6d3ca6ba5f3e723428c0c98b36b89cb28f
Omkar9867 <omkarrajan495@gmail.com>
2026-04-08 23:48:55 +0530

    fruit into baskets

---
+++ Striver Sheet/Sliding_Window_&_Two_Pointers/Problems/904. Fruit Into Baskets.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+//----------------------------------Brute Force Approach--TC->O(N^2)---------------------------
+// int totalFruit(std::vector<int>& fruits) {
+//     int maxCount = 0;
+//     for(int i = 0; i <fruits.size(); ++i){
+//         std::unordered_map<int, int> basket;
+//         int count = 0; // We just have to see from which i the count max
+//         for(int j = i; j < fruits.size(); j++){
+//             basket[fruits[j]]++;
+//             if(basket.size() > 2){
+//                 break;
+//             }
+//             count++;
+//         }
+//         maxCount = std::max(maxCount, count);
+//     }
+//     return maxCount;
+// }
+
+//----------------------------------Optimal Approach--TC->O(N)--SC->O(1)-Const '2' space--------------------------
+int totalFruit(std::vector<int>& fruits) {
+    std::unordered_map<int, int> basket;
+    int maxCount = 0;
+    int l = 0, r = 0;
+    while(r < fruits.size()){
+        basket[fruits[r]]++;
+
+        while(basket.size() > 2){
+            basket[fruits[l]]--;
+            // if count becomes zero remove from map
+            if (basket[fruits[l]] == 0) {
+                basket.erase(fruits[l]);
+            }
+            l++;
+        }
+        maxCount = std::max(maxCount, r - l+1);
+        r++;
+    }
+    return maxCount;
+}
+
+//----------------------------------Optimal Approach2--TC->O(N)--SC->O(N)---------------------------
+// int totalFruit(std::vector<int>& fruits) {
+
+// }
+
+int main(){
+    // std::vector<int> nums = {1,2,3,2,2};
+    std::vector<int> nums = {1,2,1};
+    int result = totalFruit(nums);
+
+    std::cout << "Result: " << result << std::endl;
+
+    return 0;
+}
+
+// 904. Fruit Into Baskets
+
+// You are visiting a farm that has a single row of fruit trees arranged from left to right. 
+// The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+
+// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+
+// You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+// Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+
+// Example 1:
+// Input: fruits = [1,2,1]
+// Output: 3
+// Explanation: We can pick from all 3 trees.
+
+// Example 2:
+// Input: fruits = [0,1,2,2]
+// Output: 3
+// Explanation: We can pick from trees [1,2,2].
+// If we had started at the first tree, we would only pick from trees [0,1].
+
+// Example 3:
+// Input: fruits = [1,2,3,2,2]
+// Output: 4
+// Explanation: We can pick from trees [2,3,2,2].
+// If we had started at the first tree, we would only pick from trees [1,2].
+ 
+// Constraints:
+// 1 <= fruits.length <= 105
+// 0 <= fruits[i] < fruits.length
