c0f767aefd4a4e0bd1cb77bfbad86cc1a7176fa3
Omkar9867 <omkarrajan495@gmail.com>
2026-04-09 20:43:37 +0530

    fruit into basket optimal new approach

--- Striver Sheet/Sliding_Window_&_Two_Pointers/Problems/904. Fruit Into Baskets.cpp
+++ Striver Sheet/Sliding_Window_&_Two_Pointers/Problems/904. Fruit Into Baskets.cpp
@@ -18,32 +18,60 @@
 //     return maxCount;
 // }
 
-//----------------------------------Optimal Approach--TC->O(N)--SC->O(1)-Const '2' space--------------------------
-int totalFruit(std::vector<int>& fruits) {
-    std::unordered_map<int, int> basket;
-    int maxCount = 0;
-    int l = 0, r = 0;
-    while(r < fruits.size()){
-        basket[fruits[r]]++;
-
-        while(basket.size() > 2){
-            basket[fruits[l]]--;
-            // if count becomes zero remove from map
-            if (basket[fruits[l]] == 0) {
-                basket.erase(fruits[l]);
-            }
-            l++;
-        }
-        maxCount = std::max(maxCount, r - l+1);
-        r++;
-    }
-    return maxCount;
-}
-
-//----------------------------------Optimal Approach2--TC->O(N)--SC->O(N)---------------------------
+//----------------------------------Optimal Approach--TC->O(2N)--SC->O(1)-Const '2' space--------------------------
 // int totalFruit(std::vector<int>& fruits) {
+//     std::unordered_map<int, int> basket;
+//     int maxCount = 0;
+//     int l = 0, r = 0;
+//     while(r < fruits.size()){
+//         basket[fruits[r]]++;
 
+//         while(basket.size() > 2){
+//             basket[fruits[l]]--;
+//             // if count becomes zero remove from map
+//             if (basket[fruits[l]] == 0) {
+//                 basket.erase(fruits[l]);
+//             }
+//             l++;
+//         }
+//         maxCount = std::max(maxCount, r - l+1);
+//         r++;
+//     }
+//     return maxCount;
 // }
+
+//----------------------------------Optimal Approach2--TC->O(N)--SC->O(1)---------------------------
+int totalFruit(std::vector<int>& fruits) {
+    int maxlen = 0;
+    
+    int lastfruit = -1, secondlastfruit = -1;
+    
+    // Count of current window size and the last fruit streak
+    int currcount = 0, lastfruitstreak = 0;
+
+    for (int fruit : fruits) {
+        
+        // If current fruit is same as last or second last, increase window
+        if (fruit == lastfruit || fruit == secondlastfruit) {
+            currcount++;
+        } else {
+            // Reset window size to size of last fruit streak + 1 (current fruit)
+            currcount = lastfruitstreak + 1;
+        }
+
+        // Update streak count
+        if (fruit == lastfruit) {
+            lastfruitstreak++;
+        } else {
+            lastfruitstreak = 1;
+            secondlastfruit = lastfruit;
+            lastfruit = fruit;
+        }
+        maxlen = std::max(maxlen, currcount);
+    }
+
+    return maxlen;
+}
 
 int main(){
     // std::vector<int> nums = {1,2,3,2,2};
