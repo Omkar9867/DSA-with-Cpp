dd1aaf0a9a43c6f002aa9fb2017fabdf005c2567
Omkar9867 <omkarrajan495@gmail.com>
2026-07-05 19:52:04 +0530

    job sequencing problem

---
+++ Striver Sheet/Greedy Algorithms/Problems/Job Sequencing Problem.cpp
@@ -0,0 +1,91 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Job {
+   int id; 
+   int dead;
+   int profit; 
+};
+
+class Solution {
+   public:
+      bool static comparison(Job a, Job b) {
+         return (a.profit > b.profit); 
+      }
+
+      pair < int, int > JobScheduling(Job arr[], int n) {
+        sort(arr, arr + n, comparison);
+
+        int maxi = arr[0].dead;
+        for (int i = 1; i < n; i++) {
+            maxi = max(maxi, arr[i].dead);  
+        }
+
+        int slot[maxi + 1]; //Slot arr with maxi size
+        for (int i = 0; i <= maxi; i++)
+            // Mark all slots as unoccupied initially
+            slot[i] = -1;  
+
+        int countJobs = 0, jobProfit = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = arr[i].dead; j > 0; j--) {
+                if (slot[j] == -1) {  
+                    // Assign the job to the slot
+                    slot[j] = i;  
+                    // Increment the number of jobs done
+                    countJobs++;  
+                    // Add the profit of the job
+                    jobProfit += arr[i].profit;  
+                    break;
+                }
+            }
+        }
+        return make_pair(countJobs, jobProfit);
+    }
+};
+
+
+int main() {
+    int n = 4;
+    Job arr[n] = {{1,4,20},{2,1,10},{3,2,40},{4,2,30}};
+
+    Solution ob;
+    pair < int, int > ans = ob.JobScheduling(arr, n);
+
+    cout << ans.first << " " << ans.second << endl;
+
+    return 0;
+}
+
+// Job Sequencing Problem
+
+// Problem Statement: 
+// You are given a set of N jobs where each job comes with a deadline and profit. The profit can only be earned upon completing the job within its deadline. 
+// Find the number of jobs done and the maximum profit that can be obtained. Each job takes a single unit of time and only one job can be performed at a time.
+
+// Examples
+
+// Example 1:
+// Input: 
+// N = 4, Jobs = {(1, 4, 20), (2, 1, 10), (3, 1, 40), (4, 1, 30)}  
+// Output:
+//  2 60  
+// Explanation:
+  
+// - The 3rd job with a deadline of 1 is performed during the first unit of time.  
+// - The 1st job is performed during the second unit of time as its deadline is 4.  
+// Profit = 40 + 20 = 60.  
+// So, the result is 2 jobs with a total profit of 60.
+
+// Example 2:
+// Input:
+  // N = 5, Jobs = {(1, 2, 100), (2, 1, 19), (3, 2, 27), (4, 1, 25), (5, 1, 15)}  
+// Output:
+//  2 127  
+// Explanation:
+  
+// The first and third jobs, both having a deadline of 2, give the highest profit.  
+// Profit = 100 + 27 = 127.  
+// So, the result is 2 jobs with a total profit of 127.
