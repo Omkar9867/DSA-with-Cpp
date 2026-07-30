fa4c51d21711994d173b1d45b9eeecc5d196a613
Omkar9867 <omkarrajan495@gmail.com>
2026-07-30 22:46:05 +0530

    Nodes in java

---
+++ Striver Sheet/Binary Trees/Introduction/Preorder Inorder Postorder Traversals in One Traversal.cpp
@@ -0,0 +1,18 @@
+
+
+// Preorder Inorder Postorder Traversals in One Traversal
+
+// Problem Statement: Given the root of a Binary Tree, return the preorder, inorder and postorder traversal sequence of the given tree by making just one traversal.
+
+// Examples
+
+// Input: Binary Tree: 4 2 5 3 -1 7 6 -1 9 -1 -1 8 -1 1
+// ./Preorder Inorder Postorder Traversals in One Traversal1.png
+// Output: Preorder: [4, 2, 3, 9, 1, 5, 7, 6, 8], Inorder: [3, 1, 9, 2, 4, 7, 5, 8, 6], Postorder: [1, 9, 3, 2, 7, 8, 6, 5, 4] 
+// Explanation:
+// ./Preorder Inorder Postorder Traversals in One Traversal2.png
+
+// Input: Binary Tree: 1 2 3 4 5 6 7 -1 -1 8 -1 -1 -1 9 10
+
+// Output: Preorder: [1, 2, 4, 5, 8, 3, 6, 7, 9, 10], Inorder: [4, 2, 8, 5, 1, 6, 3, 9, 7, 10], Postorder: [4, 8, 5, 2, 6, 9, 10, 7, 3, 1] 
+// Explanation: The three traversal of the tree are given above.
